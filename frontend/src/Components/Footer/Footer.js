69a354904b2bfcaa1e83fe156a484d5f87e34c2a
CB9611 <cameron.branch@selju.edu>
2024-09-09 15:34:20 -0500

    Work on Footer

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
+import ParagraphFooter from './ParagraphFooter.jsx';
 
 function Footer() {
     return (
         <div className="footer-container">
             <div className="footer">
-                THIS IS THE FOOTER
+                <div className="paragraph-footer">
+                    <ParagraphFooter />
+                </div>
+                <div className="footer-links">
+
+                </div>
             </div>
         </div>
     )
---
+++ frontend/src/Components/Footer/ParagraphFooter.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ParagraphFooter = ({ text }) => {
+    return (
+        <p>The Elder Scrolls is a series of action role-playing video games set in the expansive and richly detailed fantasy world of Tamriel. The series is known for its open-ended gameplay and intricate lore. Each game is set in a different province of Tamriel and explores the complex history, politics, and cultures of the land. Players assume the role of a customizable protagonist, embarking on quests that range from epic battles to personal stories, while interacting with a dynamic world filled with diverse races, factions, and magical elements. The Elder Scrolls games are celebrated for their deep narrative, immersive environments, and the freedom they offer players to shape their own adventures within a living, breathing world.</p>
+    );
+};
+
+export default ParagraphFooter;
