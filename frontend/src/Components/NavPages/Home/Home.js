712e05765a73147ae306103e045aa29b2132d324
CB9611 <cameron.branch@selju.edu>
2024-10-14 15:00:12 -0500

    Improved homepage

--- frontend/src/Components/NavPages/Home/Home.js
+++ frontend/src/Components/NavPages/Home/Home.js
@@ -1,12 +1,28 @@
 import './Home.css';
+import { React, useState } from 'react';
 import titleImg from '../../Images/es_title.png';
 
 function Home() {
+  const [ signUp, setmode ] = useState(false);
+  const toggleSignUp = () => {
+    setmode(!signUp);
+  };
+
   return (
     <div className="home-container">
-      <body className="home-title">
-        <img className="home-title-image" src={titleImg} />
-      </body>
+      <div className="landingpage-separator">
+        <body className="home-title">
+          <img className="home-title-image" src={titleImg} />
+          <button className="home-signup-button" onClick={ toggleSignUp }>
+            {signUp ? 'Close' : 'Sign Up?'}
+          </button>
+        </body>
+      </div>
+      <div className="signup-separator">
+        <div className={ signUp ? "signup-open" : "signup-close"}>
+        
+        </div>
+      </div>
     </div>
   );
 }
