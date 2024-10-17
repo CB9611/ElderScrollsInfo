e7aeb3f19fcd510b260b502ce186811043ff37fc
Cameron <cameron.branch@selu.edu>
2024-10-17 15:23:55 -0500

    Recoded and improved Navbar

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -5,154 +5,32 @@ import siteLogo from '../Images/Ouroboros.png';
 
 function Navbar() {
 
+    /* Dropdown */
     const [isOpen, setIsOpen] = useState(false);
-
-    const handleMouseEnter = () => {
-        setIsOpen(true);
-    }
-
-    const handleMouseLeave = () => {
-        setIsOpen(false);
-    }
-
-    const handleClick = () => {
-        setIsOpen(!isOpen);
-    }
-
-    const [navburgerOpen, setNavburgerOpen] = useState(false);
-
-    const toggleNavburger = () => {
-        setNavburgerOpen(!navburgerOpen)
-    }
+    const handleClick = () => { setIsOpen(!isOpen); }
 
     return (
         <div className="nav-container">
             <div className="navbar">
-                <div className="left-nav-items">
-                    <div className="">
-                        <Link className="links" to="/">
-                            <div className="nav-link">Home</div>
-                        </Link>
+                <Link className="nav-link" to="/">Home</Link>
+                <div onClick={handleClick}>
+                    <div className={isOpen ? "games-dropdown-open" : "games-dropdown-closed"}>
+                        {isOpen ? '▲' : 'Games ▼'}
                     </div>
-                    <div 
-                        className="games-dropdown-container"
-                        onMouseEnter={handleMouseEnter}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                        <div className={isOpen ? "games-dropdown-open" : "games-dropdown-closed"}>
-                            {isOpen ? '▲' : 'Games ▼'}
-                        </div>
-                        {isOpen && /* DROPDOWN MENU START */ (
-                            <div className="games-dropdown-list">
-                                <Link className="links" to="/Arena">
-                                    <div className="nav-link">Arena</div>
-                                </Link>
-                                <Link className="links" to="/Daggerfall">
-                                    <div className="nav-link">Daggerfall</div>
-                                </Link>
-                                <Link className="links" to="/Morrowind">
-                                    <div className="nav-link">Morrowind</div>
-                                </Link>
-                                <Link className="links" to="/Oblivion">
-                                    <div className="nav-link">Oblivion</div>
-                                </Link>
-                                <Link className="links" to="/Skyrim">
-                                    <div className="nav-link">Skyrim</div>
-                                </Link>
-                                <Link className="links" to="/ElderScrollsOnline">
-                                    <div className="nav-link">Online</div>
-                                </Link>
-                                <div className="dropdown-divider" />
-                                <Link className="links" to="/Games">
-                                    <div className="nav-link">All Games</div>
-                                </Link>
-                            </div>
-                        ) /* DROPDOWN MENU END */ }
+                    <div className={`games-dropdown-list ${isOpen ? "open" : ""}`}>
+                        <div className="dropdown-divider" />
+                        <Link className="nav-link" to="/Arena">Arena</Link>
+                        <Link className="nav-link" to="/Daggerfall">Daggerfall</Link>
+                        <Link className="nav-link" to="/Morrowind">Morrowind</Link>
+                        <Link className="nav-link" to="/Oblivion">Oblivion</Link>
+                        <Link className="nav-link" to="/Skyrim">Skyrim</Link>
+                        <Link className="nav-link" to="/ElderScrollsOnline">Online</Link>
+                        <div className="dropdown-divider" />
+                        <Link className="nav-link" to="/Games">All Games</Link>
                     </div>
                 </div>
-                <div className="navbarLogo">
-                    <Link className="siteLogo-Link" title="You are Dovah" exact to="/">
-                        <img className="siteLogo-img" src={ siteLogo } alt=""></img>
-                    </Link>
-                </div>
-                <div className="right-nav-items">
-                    <div className="nav-items">
-                        <Link className="links" to="/About">
-                            <div className="nav-link">About</div>
-                        </Link>
-                    </div>
-                    <div className="nav-items">
-                        <Link 
-                            className="links" 
-                            to="https://bethesda.net/"
-                            target="_blank" 
-                            rel="noopener noreferrer">
-                            <div className="nav-link">Bethesda</div>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-
-            {/* Mobile Navbar */}
-            <div className="navbar-mobile-container">
-                <div className={navburgerOpen ? "nav-mobileClosed" : "nav-mobileOpen"}>
-                    <ul>
-                        <Link className="links" to="/">
-                            <div className="nav-link">Home</div>
-                        </Link>
-                        <div 
-                            className="games-dropdown-container"
-                            onClick={handleClick}
-                        >
-                        <div className={isOpen ? "games-dropdown-open" : "games-dropdown-closed"}>
-                            {isOpen ? '▲' : 'Games ▼'}
-                        </div>
-                        {isOpen && /* DROPDOWN MENU START */ (
-                            <div className="games-dropdown-list">
-                                <Link className="links" to="/Arena">
-                                    <div className="nav-link">Arena</div>
-                                </Link>
-                                <Link className="links" to="/Daggerfall">
-                                    <div className="nav-link">Daggerfall</div>
-                                </Link>
-                                <Link className="links" to="/Morrowind">
-                                    <div className="nav-link">Morrowind</div>
-                                </Link>
-                                <Link className="links" to="/Oblivion">
-                                    <div className="nav-link">Oblivion</div>
-                                </Link>
-                                <Link className="links" to="/Skyrim">
-                                    <div className="nav-link">Skyrim</div>
-                                </Link>
-                                <Link className="links" to="/ElderScrollsOnline">
-                                    <div className="nav-link">Online</div>
-                                </Link>
-                                <div className="dropdown-divider" />
-                                <Link className="links" to="/Games">
-                                    <div className="nav-link">All Games</div>
-                                </Link>
-                            </div>
-                        ) /* DROPDOWN MENU END */ }
-                        </div>
-                        <Link className="links" to="/About">
-                            <div className="nav-link">About</div>
-                        </Link>
-                        <Link 
-                            className="links" 
-                            to="https://bethesda.net/"
-                            target="_blank" 
-                            rel="noopener noreferrer">
-                            <div className="nav-link">Bethesda</div>
-                        </Link>
-                    </ul>
-                </div>
-                <div className="navburger-container" onClick={toggleNavburger}>
-                    <div className="nav-hamburger" isOpen={navburgerOpen}>
-                        <div className="burger burgerTop" />
-                        <div className="burger burgerMid" />
-                        <div className="burger burgerBot" />
-                    </div>
-                </div>
+                <Link className="nav-link" to="/About">About</Link>
+                <Link className="nav-link" to="https://bethesda.net/" target="_blank" rel="noopener noreferrer">Bethesda</Link>
             </div>
         </div>
     )
